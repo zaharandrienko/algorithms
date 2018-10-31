2af12d8169b61eb86b7486c2759222a7564d847e
Zahar Andrienko <zaharandrienko88@gmail.com>
2018-10-31 20:39:01 +0200

    add dividers

--- algorithms.js
+++ algorithms.js
@@ -1,22 +1,22 @@
 
 function factorial(n) {
-    
-    if(n === 1){
+
+    if (n === 1) {
         return 1;
     } else {
-        return n * factorial(n -1);
+        return n * factorial(n - 1);
     }
 
 }
 
-function bubbleSort(arr){
+function bubbleSort(arr) {
 
-    for(let k of arr){
+    for (let k of arr) {
 
-        for(let i = 0; i < arr.length - 1; i++){
-            if(arr[i] > arr[i+1]){
-                let temp = arr[i +1];
-                arr[i +1] = arr[i];
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i + 1];
+                arr[i + 1] = arr[i];
                 arr[i] = temp;
             }
         }
@@ -27,10 +27,32 @@ function bubbleSort(arr){
 }
 
 
-function plus( a , b){
-    return a  + b  ;
+function plus(a, b) {
+    return a + b;
 }
 
-module.exports = {factorial, plus,bubbleSort};
+function max(number) {
+    let maxNum = number[0]
+    for (let cur of number) {
+        if (cur > maxNum) {
+            maxNum = cur;
+        }
+    }
+    return maxNum;
+}
+function dividers(num) {
+    numArray = []
+    for (let i = 0; i <= num / 2; i++) {
+        if (num % i == 0) {
+            numArray.push(i);
+        }
+    }
+    return numArray;
+
+}
+
+
+
+module.exports = { factorial, plus, bubbleSort, max, dividers };
 
 
--- algorithms.test.js
+++ algorithms.test.js
@@ -1,14 +1,23 @@
 const algorithms = require('./algorithms.js');
 
 test('factorial of 5 shold return 120', () => {
-    expect(algorithms.factorial(5)).toBe(120);
-  });
+  expect(algorithms.factorial(5)).toBe(120);
+});
 
 
 test('plus(3, 4) should return 7', () => {
-    expect(algorithms.plus(3, 4)).toBe(7);
-  });
+  expect(algorithms.plus(3, 4)).toBe(7);
+});
 
 test('bubble sort should work', () => {
-    expect(algorithms.bubbleSort([7,1,2, -5])).toEqual([-5, 1,2,7]);
-  });
+  expect(algorithms.bubbleSort([7, 1, 2, -5])).toEqual([-5, 1, 2, 7]);
+});
+
+test('max should work', () => {
+  expect(algorithms.max([7, 1, 2])).toEqual(7);
+});
+
+test('dividers should work', () => {
+  expect(algorithms.dividers(10)).toEqual([1, 2, 5]);
+});
+
