f3bc146d3215d6f027c1b3ff23561c772a10805c
Zahar Andrienko <zaharandrienko88@gmail.com>
2019-01-20 21:01:47 +0200

    add mergSort

--- algorithms.js
+++ algorithms.js
@@ -123,8 +123,42 @@ function binarySearch(inputArray, num) {
 
 }
 
+function mergeSort (arr) {
+    if (arr.length === 1) {
+      return arr
+    }
+  
+    const middle = Math.floor(arr.length / 2)
+    const left = arr.slice(0, middle) 
+    const right = arr.slice(middle) 
+  
+    const sortedLeft = mergeSort(left);
+    const sortedRigth =  mergeSort(right); 
+    return merge(sortedLeft ,sortedRigth  );
 
-module.exports = { factorial, plus, bubbleSort, max, dividers, checkPalindrome, adjacentElementsProduct, binarySearch };
+  }
+  
+  function merge (left, right) {
+    let result = []
+    let indexLeft = 0
+    let indexRight = 0
+
+  
+    while (indexLeft < left.length && indexRight < right.length) {
+      if (left[indexLeft] < right[indexRight]) {
+        result.push(left[indexLeft])
+        indexLeft++
+      } else {
+        result.push(right[indexRight])
+        indexRight++
+      }
+    }
+  
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+  }
+
+
+module.exports = { factorial, plus, bubbleSort, max, dividers, checkPalindrome, adjacentElementsProduct, binarySearch, mergeSort };
 
 
 
